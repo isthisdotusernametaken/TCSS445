b110396a9eb717a11f6fc1754565171e37365590
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2023-05-22 15:46:21 -0700

    Completed View Products scenario

--- chem_db_script.sql
+++ chem_db_script.sql
@@ -4,6 +4,8 @@ USE tomlin_trevor_db;
 GO
 
 
+CREATE TYPE STRING FROM NVARCHAR(128);
+GO
 
 ------------------------------
 -- Tables - Start
@@ -18,10 +20,10 @@ CREATE TABLE CUSTOMER (
     EmailAddress NVARCHAR(320) NOT NULL UNIQUE,
     PasswordHash BINARY(512) NOT NULL,
     PasswordSalt BINARY(512) NOT NULL,
-    FirstName NVARCHAR(128) NOT NULL,
-    LastName NVARCHAR(128) NOT NULL,
-    AddressLine1 NVARCHAR(128) NOT NULL,
-    AddressLine2 NVARCHAR(128) NOT NULL,
+    FirstName STRING NOT NULL,
+    LastName STRING NOT NULL,
+    AddressLine1 STRING NOT NULL,
+    AddressLine2 STRING NOT NULL,
     ZIPCode INT NOT NULL FOREIGN KEY
 		REFERENCES ZIPCODE(ZIPCode),
     AccountCreationDate DATE NOT NULL
@@ -29,7 +31,7 @@ CREATE TABLE CUSTOMER (
 
 CREATE TABLE DISCOUNT (
     DiscountID INT PRIMARY KEY IDENTITY(0, 1) NOT NULL,
-    DiscountName NVARCHAR(128) NOT NULL,
+    DiscountName STRING NOT NULL,
     Percentage DECIMAL(5, 2) NOT NULL,
     Reusability BIT NOT NULL,
     InitialValidDate DATE NOT NULL,
@@ -53,27 +55,27 @@ CREATE TABLE ONLINE_TRANSATION (
 );
 
 CREATE TABLE STATE_OF_MATTER (
-    StateOfMatterName NVARCHAR(128) PRIMARY KEY
+    StateOfMatterName STRING PRIMARY KEY
 );
 
 CREATE TABLE MEASUREMENT_UNIT (
-    MeasurementUnitName NVARCHAR(128) PRIMARY KEY,
+    MeasurementUnitName STRING PRIMARY KEY,
     MeasurementUnitAbbreviation NVARCHAR(10) UNIQUE -- NULL if no abbreviation
 );
 
 CREATE TABLE MEASUREMENT_UNIT_APPLICABILITY (
-    MeasurementUnitName NVARCHAR(128) FOREIGN KEY
+    MeasurementUnitName STRING FOREIGN KEY
 		REFERENCES MEASUREMENT_UNIT(MeasurementUnitName),
-    StateOfMatterName NVARCHAR(128) FOREIGN KEY
+    StateOfMatterName STRING FOREIGN KEY
 		REFERENCES STATE_OF_MATTER(StateOfMatterName),
     PRIMARY KEY (MeasurementUnitName, StateOfMatterName)
 );
 
 CREATE TABLE CHEMICAL_TYPE (
     ChemicalTypeID INT PRIMARY KEY IDENTITY(0, 1),
-    ChemicalName NVARCHAR(128) NOT NULL,
-    MeasurementUnitName NVARCHAR(128) NOT NULL,
-    StateOfMatterName NVARCHAR(128) NOT NULL,
+    ChemicalName STRING NOT NULL,
+    MeasurementUnitName STRING NOT NULL,
+    StateOfMatterName STRING NOT NULL,
 	FOREIGN KEY (MeasurementUnitName, StateOfMatterName)
 		REFERENCES MEASUREMENT_UNIT_APPLICABILITY(MeasurementUnitName, StateOfMatterName),
 	CONSTRAINT UQ_Name_Unit_State UNIQUE (ChemicalName, MeasurementUnitName, StateOfMatterName)
@@ -89,7 +91,7 @@ CREATE TABLE CHEMICAL_QUALITY (
 
 CREATE TABLE DISTRIBUTOR (
     DistributorID INT PRIMARY KEY IDENTITY(0, 1),
-    DistributorName NVARCHAR(128) NOT NULL UNIQUE
+    DistributorName STRING NOT NULL UNIQUE
 );
 
 CREATE TABLE SHIPMENT (
@@ -104,8 +106,8 @@ CREATE TABLE CHEMICAL (
     ChemicalID INT PRIMARY KEY IDENTITY(0, 1),
     ChemicalTypeID INT NOT NULL,
     Purity DECIMAL(6, 3) NOT NULL,
-    InitialQuantity DECIMAL(10, 2) NOT NULL,
-    RemainingQuantity DECIMAL(10, 2) NOT NULL,
+    InitialQuantity DECIMAL(44, 4) NOT NULL,
+    RemainingQuantity DECIMAL(44, 4) NOT NULL,
     ShipmentID INT NOT NULL FOREIGN KEY
 		REFERENCES SHIPMENT(ShipmentID),
     TotalPurchasePrice DECIMAL(10, 2) NOT NULL,
@@ -118,7 +120,7 @@ CREATE TABLE TRANSACTION_LINE_ITEM (
 		REFERENCES [TRANSACTION](TransactionID),
     ChemicalID INT FOREIGN KEY
 		REFERENCES CHEMICAL(ChemicalID),
-    Quantity DECIMAL(10, 2) NOT NULL,
+    Quantity DECIMAL(44, 4) NOT NULL,
     CostPerUnitWhenPurchased DECIMAL(10, 2) NOT NULL,
 	PRIMARY KEY (TransactionID, ChemicalID)
 );
@@ -145,16 +147,13 @@ CREATE TABLE REVIEW (
 -- Scenarios - Start
 ------------------------------
 
-GO
-CREATE TYPE STRINGLIST AS TABLE (String NVARCHAR(128));
-
 -- 1 (Register)
 /* The password hash and salt are to be provided by server-side code, based on
    the password text provided by the user. */
 GO
 CREATE PROCEDURE RegisterCustomer	@EmailAddress NVARCHAR(320), @PasswordHash BINARY(32), @PasswordSalt BINARY(32),
-									@FirstName NVARCHAR(128), @LastName NVARCHAR(128),
-									@AddressLine1 NVARCHAR(128), @AddressLine2 NVARCHAR(128), @ZIPCode INT
+									@FirstName STRING, @LastName STRING,
+									@AddressLine1 STRING, @AddressLine2 STRING, @ZIPCode INT
 AS
 	
 INSERT INTO CUSTOMER (EmailAddress, PasswordHash, PasswordSalt,
@@ -169,10 +168,57 @@ VALUES (@EmailAddress, @PasswordHash, @PasswordSalt,
 
 -- 2 (View Products)
 GO
-CREATE FUNCTION SearchProducts	(@ChemicalName NVARCHAR(128),
+CREATE FUNCTION AverageRating	(@ChemicalID INT) -- Helper
+RETURNS TABLE AS RETURN (
+	SELECT		AVG(R.Stars)
+	FROM		REVIEW R
+	WHERE		@ChemicalID = R.ChemicalID
+);
+
+GO
+CREATE FUNCTION PurchaserCount	(@ChemicalID INT) -- Helper
+RETURNS TABLE AS RETURN (
+	SELECT		SUM(DISTINCT T.CustomerID)
+	FROM		TRANSACTION_LINE_ITEM TL, [TRANSACTION] T
+	WHERE		T.TransactionID = TL.TransactionID
+		AND		@ChemicalID = TL.ChemicalID
+);
+
+GO
+CREATE FUNCTION SearchProducts	(@ResultsPosition INT, @ResultsCount INT,
+								 @ChemicalName STRING,
 								 @MinPurity DECIMAL(6, 3), @MaxPurity DECIMAL(6, 3),
-								 @StatesOfMatter STRINGLIST, @Distributors STRINGLIST,
-								 @FirstSortBy CHAR, @SecondSortBy CHAR, @ThirdSortBy CHAR, @FourthSortBy CHAR)
+								 @StateOfMatter STRING, @Distributor STRING,
+								 @FirstSortBy CHAR, @SecondSortBy CHAR, @ThirdSortBy CHAR, @FourthSortBy CHAR,
+								 @FirstSortAsc BIT, @SecondSortAsc BIT, @ThirdSortAsc BIT, @FourthSortAsc BIT)
+RETURNS TABLE AS RETURN (
+	SELECT		CT.ChemicalName, CQ.Purity, CT.StateOfMatterName,
+				C.RemainingQuantity, CQ.CostPerUnit,
+				M.MeasurementUnitName, M.MeasurementUnitAbbreviation,
+				D.DistributorName
+	FROM		CHEMICAL C, CHEMICAL_TYPE CT, CHEMICAL_QUALITY CQ,
+				MEASUREMENT_UNIT M, SHIPMENT S, DISTRIBUTOR D
+	WHERE		C.ChemicalTypeID = CT.ChemicalTypeID -- Match C and CT
+		AND		C.ChemicalTypeID = CQ.ChemicalTypeID AND C.Purity = CQ.Purity -- Match C and CQ
+		AND		CT.MeasurementUnitName = M.MeasurementUnitName -- Match CT and M
+		AND		C.ShipmentID = S.ShipmentID -- Match C and S
+		AND		(@ChemicalName IS NULL OR (CT.ChemicalName LIKE '%' + @ChemicalName + '%')) -- If name given, require it in chem names
+		AND		(@MinPurity = @MaxPurity OR (C.Purity BETWEEN @MinPurity AND @MaxPurity)) -- Require purity range
+		AND		(@StateOfMatter IS NULL OR (CT.StateOfMatterName = @StateOfMatter)) -- If state given, require match
+		AND		(@Distributor IS NULL OR (D.DistributorName = @Distributor)) -- If distributor given, require match
+	ORDER BY
+		CASE WHEN @FirstSortBy = 'C' AND @FirstSortAsc = 1 THEN CQ.CostPerUnit END ASC, -- Cost
+		CASE WHEN @FirstSortBy = 'C' AND @FirstSortAsc = 0 THEN CQ.CostPerUnit END DESC,
+		CASE WHEN @FirstSortBy = 'P' AND @FirstSortAsc = 1 THEN CQ.Purity END ASC, -- Purity
+		CASE WHEN @FirstSortBy = 'P' AND @FirstSortAsc = 0 THEN CQ.Purity END DESC,
+		CASE WHEN @FirstSortBy = 'R' AND @FirstSortAsc = 1 THEN AverageRating(@ChemicalID) END ASC, -- Rating
+		CASE WHEN @FirstSortBy = 'R' AND @FirstSortAsc = 0 THEN AverageRating(@ChemicalID) END DESC,
+		CASE WHEN @FirstSortBy = 'N' AND @FirstSortAsc = 1 THEN PurchaserCount(@ChemicalID) END ASC, -- Number of purchasers
+		CASE WHEN @FirstSortBy = 'N' AND @FirstSortAsc = 0 THEN PurchaserCount(@ChemicalID) END DESC
+	OFFSET @ResultsPosition ROWS
+	FETCH NEXT @ResultsCount ROWS ONLY
+);
+
 
 
 ------------------------------
