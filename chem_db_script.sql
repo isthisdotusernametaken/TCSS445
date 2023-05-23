332712d1f3a10a29dc9cf8b7b0f4caa5507f60cd
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2023-05-23 04:33:53 +0000

    Removed ids

--- chem_db_script.sql
+++ chem_db_script.sql
@@ -531,64 +531,64 @@ GO
 ------------------------------
 
 -- Distributors
-INSERT INTO DISTRIBUTOR (DistributorID, DistributorName)
-VALUES ('0', 'ABC Distributors');
+INSERT INTO DISTRIBUTOR (DistributorName)
+VALUES ('ABC Distributors');
 
-INSERT INTO DISTRIBUTOR (DistributorID, DistributorName)
-VALUES ('1', 'Chemical Creators');
+INSERT INTO DISTRIBUTOR (DistributorName)
+VALUES ('Chemical Creators');
 
-INSERT INTO DISTRIBUTOR (DistributorID, DistributorName)
-VALUES ('2', 'Chemistry Inc.');
+INSERT INTO DISTRIBUTOR (DistributorName)
+VALUES ('Chemistry Inc.');
 
 -- Shipments
-INSERT INTO SHIPMENT (ShipmentID, DistributorID, PurchaseDate, ReceiveDate)
-VALUES ('1001', '0', '2023-05-21', '2023-05-22');
+INSERT INTO SHIPMENT (DistributorID, PurchaseDate, ReceiveDate)
+VALUES ('0', '2023-05-21', '2023-05-22');
 
-INSERT INTO SHIPMENT (ShipmentID, DistributorID, PurchaseDate, ReceiveDate)
-VALUES ('1002', '1', '2023-05-23', '2023-05-24');
+INSERT INTO SHIPMENT (DistributorID, PurchaseDate, ReceiveDate)
+VALUES ('1', '2023-05-23', '2023-05-24');
 
-INSERT INTO SHIPMENT (ShipmentID, DistributorID, PurchaseDate, ReceiveDate)
-VALUES ('1003', '2', '2023-05-25', '2023-05-26');
+INSERT INTO SHIPMENT (DistributorID, PurchaseDate, ReceiveDate)
+VALUES ('2', '2023-05-25', '2023-05-26');
 
 -- Discounts
-INSERT INTO DISCOUNT (DiscountID, DiscountName, Percentage, Reusability, InitialValidDate, ExpirationDate)
-VALUES ('1', 'Summer Sale', 20, 1, '2023-06-01', '2023-06-30');
+INSERT INTO DISCOUNT (DiscountName, Percentage, Reusability, InitialValidDate, ExpirationDate)
+VALUES ('Summer Sale', 20, 1, '2023-06-01', '2023-06-30');
 
-INSERT INTO DISCOUNT (DiscountID, DiscountName, Percentage, Reusability, InitialValidDate, ExpirationDate)
-VALUES ('2', 'Holiday Special', 15, 0, '2023-12-01', '2023-12-31');
+INSERT INTO DISCOUNT (DiscountName, Percentage, Reusability, InitialValidDate, ExpirationDate)
+VALUES ('Holiday Special', 15, 0, '2023-12-01', '2023-12-31');
 
-INSERT INTO DISCOUNT (DiscountID, DiscountName, Percentage, Reusability, InitialValidDate, ExpirationDate)
-VALUES ('3', 'New Year Discount', 10, 1, '2024-01-01', '2024-01-31');
+INSERT INTO DISCOUNT (DiscountName, Percentage, Reusability, InitialValidDate, ExpirationDate)
+VALUES ('New Year Discount', 10, 1, '2024-01-01', '2024-01-31');
 
 -- Transactions
-INSERT INTO TRANSACTION (TransactionID, CustomerID, PurchaseDate, TaxAmount, DiscountID)
-VALUES ('2001', '0', '2023-05-21', 10.50, '1');
+INSERT INTO TRANSACTION (CustomerID, PurchaseDate, TaxAmount, DiscountID)
+VALUES ('0', '2023-05-21', 10.50, '1');
 
-INSERT INTO TRANSACTION (TransactionID, CustomerID, PurchaseDate, TaxAmount, DiscountID)
-VALUES ('2002', '1', '2023-05-22', 5.75, '2');
+INSERT INTO TRANSACTION (CustomerID, PurchaseDate, TaxAmount, DiscountID)
+VALUES ('1', '2023-05-22', 5.75, '2');
 
-INSERT INTO TRANSACTION (TransactionID, CustomerID, PurchaseDate, TaxAmount, DiscountID)
-VALUES ('2003', '2', '2023-05-23', 8.20, '3');
+INSERT INTO TRANSACTION (CustomerID, PurchaseDate, TaxAmount, DiscountID)
+VALUES ('2', '2023-05-23', 8.20, '3');
 
 -- Online Transactions
 INSERT INTO ONLINE_TRANSACTION (TransactionID, ReceiveDate)
-VALUES ('2001', '2023-05-21');
+VALUES ('0', '2023-05-21');
 
 INSERT INTO ONLINE_TRANSACTION (TransactionID, ReceiveDate)
-VALUES ('2002', '2023-05-22');
+VALUES ('1', '2023-05-22');
 
 INSERT INTO ONLINE_TRANSACTION (TransactionID, ReceiveDate)
-VALUES ('2003', '2023-05-23');
+VALUES ('2', '2023-05-23');
 
 -- Transaction Line Item
 INSERT INTO TRANSACTION_LINE_ITEM (TransactionID, ChemicalID, Quantity, CostPerUnitWhenPurchased)
-VALUES ('2001', '1001', 5, 10.99);
+VALUES ('0', '1001', 5, 10.99);
 
 INSERT INTO TRANSACTION_LINE_ITEM (TransactionID, ChemicalID, Quantity, CostPerUnitWhenPurchased)
-VALUES ('2002', '1002', 2, 7.99);
+VALUES ('1', '1002', 2, 7.99);
 
 INSERT INTO TRANSACTION_LINE_ITEM (TransactionID, ChemicalID, Quantity, CostPerUnitWhenPurchased)
-VALUES ('2003', '1003', 10, 15.99);
+VALUES ('2', '1003', 10, 15.99);
 
 -- States of Matter
 INSERT INTO STATES_OF_MATTER (StateOfMatterName)
@@ -621,44 +621,44 @@ INSERT INTO MEASUREMENT_UNIT_APPLICABILITY (MeasurementUnitName, StateOfMatterNa
 VALUES ('Kilogram', 'Solid');
 
 -- Chemical Types
-INSERT INTO CHEMICAL_TYPE (ChemicalTypeID, ChemicalName, MeasurementUnitName, StateOfMatterName)
-VALUES ('1', 'Acetone', 'Milliliter', 'Liquid');
+INSERT INTO CHEMICAL_TYPE (ChemicalName, MeasurementUnitName, StateOfMatterName)
+VALUES ('Acetone', 'Milliliter', 'Liquid');
 
-INSERT INTO CHEMICAL_TYPE (ChemicalTypeID, ChemicalName, MeasurementUnitName, StateOfMatterName)
-VALUES ('2', 'Sodium Chloride', 'Gram', 'Solid');
+INSERT INTO CHEMICAL_TYPE (ChemicalName, MeasurementUnitName, StateOfMatterName)
+VALUES ('Sodium Chloride', 'Gram', 'Solid');
 
-INSERT INTO CHEMICAL_TYPE (ChemicalTypeID, ChemicalName, MeasurementUnitName, StateOfMatterName)
-VALUES ('3', 'Ethanol', 'Milliliter', 'Liquid');
+INSERT INTO CHEMICAL_TYPE (ChemicalName, MeasurementUnitName, StateOfMatterName)
+VALUES ('Ethanol', 'Milliliter', 'Liquid');
 
 -- Chemical Qualities
 INSERT INTO CHEMICAL_QUALITY (ChemicalTypeID, Purity, CostPerUnit)
-VALUES ('1', '99.9%', 5.99);
+VALUES ('0', '99.9%', 5.99);
 
 INSERT INTO CHEMICAL_QUALITY (ChemicalTypeID, Purity, CostPerUnit)
-VALUES ('2', '98.5%', 2.99);
+VALUES ('1', '98.5%', 2.99);
 
 INSERT INTO CHEMICAL_QUALITY (ChemicalTypeID, Purity, CostPerUnit)
-VALUES ('3', '99.5%', 3.99);
+VALUES ('2', '99.5%', 3.99);
 
 -- Chemicals
-INSERT INTO CHEMICAL (ChemicalID, ChemicalTypeID, Purity, InitialQuantity, RemainingQuantity, ShipmentID, TotalPurchasePrice)
-VALUES ('C1', 'CT1', '99.9%', 100, 100, 'S1', 599.00);
+INSERT INTO CHEMICAL (ChemicalTypeID, Purity, InitialQuantity, RemainingQuantity, ShipmentID, TotalPurchasePrice)
+VALUES ('0', '99.9%', 100, 100, 'S1', 599.00);
 
-INSERT INTO CHEMICAL (ChemicalID, ChemicalTypeID, Purity, InitialQuantity, RemainingQuantity, ShipmentID, TotalPurchasePrice)
-VALUES ('C2', 'CT2', '98.5%', 200, 150, 'S2', 449.00);
+INSERT INTO CHEMICAL (ChemicalTypeID, Purity, InitialQuantity, RemainingQuantity, ShipmentID, TotalPurchasePrice)
+VALUES ('1', '98.5%', 200, 150, 'S2', 449.00);
 
-INSERT INTO CHEMICAL (ChemicalID, ChemicalTypeID, Purity, InitialQuantity, RemainingQuantity, ShipmentID, TotalPurchasePrice)
-VALUES ('C3', 'CT3', '99.5%', 50, 50, 'S3', 199.50);
+INSERT INTO CHEMICAL (ChemicalTypeID, Purity, InitialQuantity, RemainingQuantity, ShipmentID, TotalPurchasePrice)
+VALUES ('2', '99.5%', 50, 50, 'S3', 199.50);
 
 -- Reviews
-INSERT INTO REVIEW (ReviewID, TransactionID, ChemicalID, Stars, Text, ReviewDate)
-VALUES ('1', '1', '1', 5, 'Excellent product!', '2023-05-01');
+INSERT INTO REVIEW (TransactionID, ChemicalID, Stars, Text, ReviewDate)
+VALUES ('1', '1', 5, 'Excellent product!', '2023-05-01');
 
-INSERT INTO REVIEW (ReviewID, TransactionID, ChemicalID, Stars, Text, ReviewDate)
-VALUES ('2', '2', '2', 4, 'Good quality, but expensive.', '2023-05-05');
+INSERT INTO REVIEW (TransactionID, ChemicalID, Stars, Text, ReviewDate)
+VALUES ('2', '2', 4, 'Good quality, but expensive.', '2023-05-05');
 
-INSERT INTO REVIEW (ReviewID, TransactionID, ChemicalID, Stars, Text, ReviewDate)
-VALUES ('3', '3', '3', 3, 'Average product, needs improvement.', '2023-05-10');
+INSERT INTO REVIEW (TransactionID, ChemicalID, Stars, Text, ReviewDate)
+VALUES ('3', '3', 3, 'Average product, needs improvement.', '2023-05-10');
 
 ------------------------------
 -- Example Data - End
