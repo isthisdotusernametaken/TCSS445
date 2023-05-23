92169c7b4eea7eacda9ec691654f57f74296ed1c
Joshua Barbee <joshuaa.bbarbee@gmail.com>
2023-05-22 22:41:49 -0700

    Completed scenarios for viewing purchases and subpurchases Started scenarios for transactions

--- chem_db_script.sql
+++ chem_db_script.sql
@@ -49,7 +49,7 @@ CREATE TABLE [TRANSACTION] (
 		REFERENCES DISCOUNT(DiscountID)
 );
 
-CREATE TABLE ONLINE_TRANSATION (
+CREATE TABLE ONLINE_TRANSACTION (
     TransactionID INT PRIMARY KEY FOREIGN KEY
 		REFERENCES [TRANSACTION](TransactionID),
     ReceiveDate DATE NOT NULL
@@ -111,7 +111,7 @@ CREATE TABLE CHEMICAL (
     RemainingQuantity DECIMAL(38, 4) NOT NULL,
     ShipmentID INT NOT NULL FOREIGN KEY
 		REFERENCES SHIPMENT(ShipmentID),
-    TotalPurchasePrice DECIMAL(10, 2) NOT NULL,
+    TotalPurchasePrice DECIMAL(10, 2) NOT NULL, -- Purchase cost from distributor
 	FOREIGN KEY (ChemicalTypeID, Purity)
 		REFERENCES CHEMICAL_QUALITY(ChemicalTypeID, Purity)
 );
@@ -199,7 +199,8 @@ CREATE OR ALTER FUNCTION SearchProducts	(@ResultsPosition INT, @ResultsCount INT
 										 @FirstSortBy CHAR, @SecondSortBy CHAR, @ThirdSortBy CHAR, @FourthSortBy CHAR,
 										 @FirstSortAsc BIT, @SecondSortAsc BIT, @ThirdSortAsc BIT, @FourthSortAsc BIT)
 RETURNS TABLE AS RETURN ( -- A product is defined as an entry in the CHEMICAL table
-	SELECT		C.ChemicalID, CT.ChemicalName, CQ.Purity, CT.StateOfMatterName,
+	SELECT		C.ChemicalID, -- To add the product to the cart
+				CT.ChemicalName, CQ.Purity, CT.StateOfMatterName,
 				C.RemainingQuantity, CQ.CostPerUnit,
 				M.MeasurementUnitName, M.MeasurementUnitAbbreviation,
 				D.DistributorName,
@@ -238,8 +239,8 @@ RETURNS TABLE AS RETURN (
 	WHERE		R.TransactionID = T.TransactionID
 		AND		T.CustomerID = C.CustomerID
 	ORDER BY	R.ReviewDate DESC
-	OFFSET @ResultsPosition ROWS
-	FETCH NEXT @ResultsCount ROWS ONLY
+	OFFSET		@ResultsPosition ROWS
+	FETCH NEXT	@ResultsCount ROWS ONLY
 );
 
 -- S4 (Login)
@@ -265,7 +266,32 @@ BEGIN
 	);
 END
 
--- S5 (View Own Purchases)
+-- S5 (Complete Transaction)
+GO
+DROP PROCEDURE IF EXISTS CompleteTransaction;
+GO
+DROP TYPE IF EXISTS TRANSACTION_CART
+GO
+CREATE TYPE TRANSACTION_CART AS TABLE (ChemicalID INT, Quantity DECIMAL(38, 4));
+
+GO
+CREATE PROCEDURE CompleteTransaction	@CustomerID INT, @TaxAmount DECIMAL(10, 2), @DiscountID INT,
+										@Cart TRANSACTION_CART READONLY, @Online BIT
+AS
+	
+INSERT INTO CUSTOMER (EmailAddress, PasswordHash, PasswordSalt,
+					  FirstName, LastName,
+					  AddressLine1, AddressLine2, ZIPCode,
+					  AccountCreationDate)
+VALUES (@EmailAddress, @PasswordHash, @PasswordSalt,
+		@FirstName, @LastName,
+		@AddressLine1, @AddressLine2, @ZIPCode,
+		GETDATE());
+
+-- S6 (Mark Delivery Completion)
+
+
+-- S7 (View Purchases)
 /* Note: The server-side application (which, in this basic project, is not
    separated from the UI but would be in practice) stores the current user's
    CustomerID after logging in. The CustomerID should never be sent to the
@@ -273,40 +299,74 @@ END
    application for the current user's session should be used here to retrieve
    only that user's purchases. */
 GO
+CREATE OR ALTER FUNCTION PurchaseTotal	(@TransactionID INT) -- Helper
+RETURNS DECIMAL(10, 2) AS
+BEGIN
+	RETURN (
+		SELECT		(T1.TaxAmount + (1.0 - D.[Percentage]) * (
+						SELECT	SUM(TL.CostPerUnitWhenPurchased * TL.Quantity) -- Total cost of products in the transaction
+						FROM	TRANSACTION_LINE_ITEM TL
+						WHERE	TL.TransactionID = @TransactionID
+					))
+		FROM		[TRANSACTION] T1, DISCOUNT D
+		WHERE		T1.TransactionID = @TransactionID
+			AND		T1.DiscountID = D.DiscountID
+	);
+END
+
+GO
 CREATE OR ALTER FUNCTION ViewPurchasesWithoutOnlineStatus	(@ResultsPosition INT, @ResultsCount INT,
-															 @CustomerID INT, @SortAsc BIT) -- Helper
+															 @CustomerID INT, @SortNewestFirst BIT) -- Helper
 RETURNS TABLE AS RETURN (
-	SELECT		CT.ChemicalName, CH.Purity, CT.StateOfMatterName,
-				T.PurchaseDate, T.TaxAmount, T.DiscountID, TL.Quantity,
-				D.DistributorName,
-				T.TransactionID -- For determining whether the transaction was online
-	FROM		CUSTOMER CU, [TRANSACTION] T, TRANSACTION_LINE_ITEM TL,
-				CHEMICAL CH, CHEMICAL_TYPE CT, SHIPMENT S, DISTRIBUTOR D
-	WHERE		CU.CustomerID = T.CustomerID
-		AND		T.TransactionID = TL.TransactionID
-		AND		TL.ChemicalID = CH.ChemicalID
-		AND		CH.ChemicalID = CT.ChemicalTypeID
-		AND		CH.ShipmentID = S.ShipmentID
-		AND		S.DistributorID = D.DistributorID
-	ORDER BY -- Purchase date, then alphabetical by product name
-		CASE WHEN @SortAsc = 1 THEN T.PurchaseDate END ASC,
-		CASE WHEN @SortAsc = 0 THEN T.PurchaseDate END DESC,
-		CT.ChemicalName
-	OFFSET @ResultsPosition ROWS
-	FETCH NEXT @ResultsCount ROWS ONLY
+	SELECT		T.PurchaseDate, [dbo].PurchaseTotal(T.TransactionID) AS PurchaseTotal,
+				D.DiscountName, D.[Percentage],
+				T.TransactionID -- For determining whether the transaction was online and finding subpurchases
+	FROM		CUSTOMER C, [TRANSACTION] T, DISCOUNT D
+	WHERE		C.CustomerID = T.CustomerID
+		AND		T.DiscountID = D.DiscountID
+	ORDER BY
+		CASE WHEN @SortNewestFirst = 1 THEN T.PurchaseDate END ASC,
+		CASE WHEN @SortNewestFirst = 0 THEN T.PurchaseDate END DESC
+	OFFSET		@ResultsPosition ROWS
+	FETCH NEXT	@ResultsCount ROWS ONLY
 );
 
 GO
 CREATE OR ALTER FUNCTION ViewPurchases	(@ResultsPosition INT, @ResultsCount INT,
 										 @CustomerID INT, @SortAsc BIT)
 RETURNS TABLE AS RETURN (
-	SELECT		P.ChemicalName, P.Purity, P.StateOfMatterName,
-				P.PurchaseDate, P.TaxAmount, P.DiscountID, P.Quantity,
-				P.DistributorName,
+	SELECT		P.PurchaseDate, P.PurchaseTotal,
+				P.DiscountName, P.[Percentage],
+				P.TransactionID, -- For finding subpurchases
 				O.ReceiveDate -- NULL if transaction was in-person, 0 DATE if online and not delivered
 	FROM		ViewPurchasesWithoutOnlineStatus(@ResultsPosition, @ResultsCount, @CustomerID, @SortAsc) P
-		LEFT OUTER JOIN ONLINE_TRANSATION O
+		LEFT OUTER JOIN ONLINE_TRANSACTION O
 			ON	P.TransactionID = O.TransactionID
+);
+
+-- S8 (View Subpurchases/Line Items)
+/* While viewing their purchases, the user can request a detailed description
+   of a specific item included in that transaction. */
+GO
+CREATE OR ALTER FUNCTION ViewSubpurchases	(@ResultsPosition INT, @ResultsCount INT,
+											 @TransactionID INT, @SortAsc BIT)
+RETURNS TABLE AS RETURN (
+	SELECT		CT.ChemicalName, C.Purity, TL.Quantity, M.MeasurementUnitAbbreviation, CT.StateOfMatterName,
+				(TL.Quantity * TL.CostPerUnitWhenPurchased) AS Cost,
+				D.DistributorName
+	FROM		[TRANSACTION] T, TRANSACTION_LINE_ITEM TL,
+				CHEMICAL C, CHEMICAL_TYPE CT, MEASUREMENT_UNIT M,
+				SHIPMENT S, DISTRIBUTOR D
+	WHERE		@TransactionID = T.TransactionID
+		AND		T.TransactionID = TL.TransactionID
+		AND		TL.ChemicalID = C.ChemicalID
+		AND		C.ChemicalTypeID = CT.ChemicalTypeID
+		AND		CT.MeasurementUnitName = M.MeasurementUnitName
+		AND		C.ShipmentID = S.ShipmentID
+		AND		S.DistributorID = D.DistributorID
+	ORDER BY	CT.ChemicalName
+	OFFSET		@ResultsPosition ROWS
+	FETCH NEXT	@ResultsCount ROWS ONLY
 );
 
 ------------------------------
